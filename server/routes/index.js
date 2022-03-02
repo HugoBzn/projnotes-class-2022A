31730686ec72d895fb931d4d5edb411e232eb201
HugoBzn <hugobazan1499@gmail.com>
2022-03-02 07:53:12 -0600

    Adding Babel-watch

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //Render manda a renderizar (generar y entregar) ls vista al cliente
-  let emojieDataset = ['💻','👨‍💻', '🎈','🎄','🦺','🚵‍♀️','🎁'];
+  let emojieDataset = ['💻','👨‍💻', '🎈','🎄','🦺','🚵‍♀️','🎁','🚆','🌐','♥'];
   let emojie = emojieDataset[Math.floor(Math.random() * emojieDataset.length)]; 
   
   res.render('index', 
