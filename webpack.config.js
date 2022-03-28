5824848a1fbded753d508ed27a40005d182cafdf
HugoBzn <hugobazan1499@gmail.com>
2022-03-28 08:52:09 -0600

    Babel y CSS extract sin probar

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,58 @@
 //Importando dependencia path
 //dependencia del core de Node
-const path = require('path');
+const path = require("path");
+
+//Plugins para Webpack
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     //1. Especificar el archivo de entrada
-    entry: './client/index.js',
+    entry: "./client/index.js",
     //2. Especificar el archivo de salida
     output: {
         //2.1 Ruta absoluta de la salida
-        path: path.resolve(__dirname,'public'),
+        path: path.resolve(__dirname, "public"),
         //2.2 Nombre del archivo de salida
-        filename: path.join('javascripts','bundle.js'),
+        filename: path.join("javascripts", "bundle.js"),
         //2.3 Path publico
-        publicPath: '/'
-    }
-}
+        publicPath: "/",
+    },
+    //3. Modulos
+    module: {
+        rules: [
+            //3.1 Regla para Babel
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    //3.1.1 Primer stage
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                [
+                                    "@babel/preset-env",
+                                    {
+                                        modules: false,
+                                        useBuiltIns: "usage",
+                                        targets: "> 0.25%, not dead",
+                                        corejs: 3,
+                                    },
+                                ],
+                            ],
+                        },
+                    },
+                ],
+            },
+            //3.2 Reglas para CSS
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader]
+            }
+        ],
+    },
+    //4. Plugins
+    plugins: [new MiniCssExtractPlugin({
+        filename: path.join('stylesheets','styles.css')
+    })]
+};
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -22,5 +22,34 @@ module.exports = {
         port: 8080,
         //3.3 Definiendo host
         host: 'localhost'
+    },
+    //4. Modulos
+    module: {
+        rules: [
+            //4.1 Regla para Babel
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    //4.1.1 Primer stage
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                [
+                                    "@babel/preset-env",
+                                    {
+                                        modules: false,
+                                        useBuiltIns: "usage",
+                                        targets: "> 0.25%, not dead",
+                                        corejs: 3,
+                                    },
+                                ],
+                            ],
+                        },
+                    },
+                ],
+            },
+        ],
     }
 }
