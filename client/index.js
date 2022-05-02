9aaabcd53ecba9720fe766eb6585a0521816157f
HugoBzn <hugobazan1499@gmail.com>
2022-05-02 07:50:50 -0500

    S28 RBNB configurado

--- client/index.js
+++ client/index.js
@@ -1,28 +1,31 @@
-// Incorporando estilos a mi bundle
-import './styles/style.css'
-import './styles/mystyle.css'
+/* eslint-disable */
+/* eslint-disable no-console */
 
-console.log("🎁 Front-End Working!!!");
+// Incorporando estilos a mi bundle
+import './styles/style.css';
+import './styles/mystyle.css';
+
+console.log('🎁 Front-End Working!!!');
 
 // Default parameters ES6/2015
-let show = (m = "😝") => {
-    console.log(m);
+const show = (m = '😝') => {
+  console.log(m);
 };
 show();
 
 // Promises ES06
 function resolveAfter2Seconds() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("function resolve");
-        }, 10000);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('function resolve');
+    }, 10000);
+  });
 }
 
 async function asyncCall() {
-    console.log("Calling async function!!!");
-    const result = await resolveAfter2Seconds();
-    console.log(result); // Imprime "function resolve" en la consola
+  console.log('Calling async function!!!');
+  const result = await resolveAfter2Seconds();
+  console.log(result); // Imprime "function resolve" en la consola
 }
 
 asyncCall();
