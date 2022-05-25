d9ba5c354f9e7fe1fbbf915f60e86de29d0cc175
HugoBzn <hugobazan1499@gmail.com>
2022-05-25 08:05:04 -0500

    S41 Creando el ProjectModel

---
+++ server/models/ProjectModels.js
@@ -0,0 +1,25 @@
+// 1. ODM  - Mongoose
+import mongoose from 'mongoose';
+
+// 2. Desestructuro el modulo de Schemas de Mongoose
+const { Schema } = mongoose;
+
+// 3. Creamos el Schema
+const ProjectSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// 4. Generar el modelo a partir de un Schema
+// (Compilar el modelo)
+export default mongoose.model('project', ProjectSchema);
