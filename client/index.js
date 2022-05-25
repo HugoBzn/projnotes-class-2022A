d498f332176492ef5cecbe0d57dc27aa7d1c4816
HugoBzn <hugobazan1499@gmail.com>
2022-05-25 08:38:34 -0500

    S41 Agregando Menu Dropdown de Acciones

--- client/index.js
+++ client/index.js
@@ -6,7 +6,12 @@ import './styles/mystyle.css';
 
 // Inicializando elementos de Materializecss
 document.addEventListener('DOMContentLoaded', () => {
+  /* Inicializando los sidenavs */
   // Obteniendo la referencia a la barra de navegacion lateral
   const sideNavs = document.querySelectorAll('.sidenav');
   M.Sidenav.init(sideNavs);
+
+  /* Inicializando los dropdown */
+  const dropdowns = document.querySelectorAll('.dropdown-trigger');
+  M.Dropdown.init(dropdowns);
 });
